5c1639371d9f76e497f6d3e1c772af3362410b49
Michelle Martinez Bianchi <127590227+MichelleMyBad@users.noreply.github.com>
2023-05-09 20:03:55 +0200

    Add files via upload

---
+++ Isogramma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+public static class Isogramma
+{
+    public static bool Verifica(string word)
+    {
+        for(int i=0;i<word.Length-1;i++){
+            for(int j=i+1;j<word.Length;j++){
+                if(Char.IsLetter(word[i])&& Char.ToLower(word[i])== Char.ToLower(word[j])){
+                return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
